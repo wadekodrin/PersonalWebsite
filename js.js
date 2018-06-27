fda45dc3940d1906138a593d14c357b6537a1460
Wade <wade.kodrin@gmail.com>
2018-06-27 14:04:45 -0500

    second commit

---
+++ js.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $(".FEMA").click(function(){
+        $(".FEMAp").toggle(500);
+    });
+    $(".Tulane").click(function(){
+        $(".Tulanep").toggle(500);
+     });
+   	$(".SCS").click(function(){
+        $(".SCSp").toggle(500);
+    });
+	$(".LSU").click(function(){
+        $(".LSUp").toggle(500);
+    });
+    $(".BPI").click(function(){
+        $(".BPIp").toggle(500);
+    });
+  	$(".FA").click(function(){
+        $(".FAp").toggle(500);
+    });
+  	$(".MT").click(function(){
+        $(".MTp").toggle(500);
+    });
+  	$(".SCUBA").click(function(){
+        $(".SCUBAp").toggle(500);
+    });
+    $(".plexos").click(function(){
+        $(".plexosp").toggle(500);
+    });
+    $(".mir").click(function(){
+        $(".mirp").toggle(500);
+    });
+    $(".NOHSEP").click(function(){
+        $(".NOHSEPp").toggle(500);
+    });
+    $(".tul").click(function(){
+        $(".tulp").toggle(500);
+    });
+    $(".USAG").click(function(){
+        $(".USAGp").toggle(500);
+    });
+    $(".ostrd").click(function(){
+        $(".ostrdp").toggle(500);
+    });
+
+
+
+
+
+});
+
